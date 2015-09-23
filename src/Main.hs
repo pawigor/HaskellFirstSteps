dff9655f87cb62493a5d2a0ea62b654e1f34251b
Igor Pavlichenko <pawigor@komcity.ru>
2015-09-23 05:10:28 +0000

    1.1 and 1.2 module

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,19 @@
 module Main where
+--- 1.1
+main = putStrLn "Hello, world! from Main module"
 
+--- 1.2
+
+infixl 6 *+*
+
+(*+*) a b = a ^ 2 + b ^ 2
+
+l = 1 + 3 *+* 2 * 2
+
+x |*| y = abs (x-y)
+
+m = 5 |*| 7
+
+i = logBase 4 (min 20 (9 + 7))
+
+j =  logBase 4 $ min 20 $ 9 + 7
